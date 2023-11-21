a1a449a44e6c098ee2c780ce5d0d3b5e4333b13b
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-21 16:59:29 +0100

    color div

--- myscript.js
+++ myscript.js
@@ -8,15 +8,6 @@ let content;
 for (let i = 1; i <= 100; i++) {
     //console.log(i);
 
-   // se il numero è divisibile per 3 stampa "fizz", se è divisibile per 5 stampa "buzz", se è divisibile sia per 3 che per 5 stampa "fizzbuzz", se non è divisibile ne per 3 ne per 5 stampa solo il numero
-    if (i % 3 === 0)  {
-        content = "fizz";
-    } else if (i % 5 === 0) {
-        content = "buzz";
-    } else {
-        content = i;
-    }
-
     // creazione elementi tramite innerHTML
     const box = document.createElement("div");
 
@@ -28,6 +19,21 @@ for (let i = 1; i <= 100; i++) {
 
     // aggiunta dell'elelmento al container
     container.append(box);
+
+   // se il numero è divisibile per 3 stampa "fizz", se è divisibile per 5 stampa "buzz", se è divisibile sia per 3 che per 5 stampa "fizzbuzz", se non è divisibile ne per 3 ne per 5 stampa solo il numero
+   if (i % 3 === 0 && i % 5 === 0) {
+        content = "fizz.buzz";
+        box.classList.add("fizz_buzz")
+    } else if (i % 3 === 0)  {
+        content = "fizz";
+        box.classList.add("fizz");
+    } else if (i % 5 === 0) {
+        content = "buzz";
+        box.classList.add("buzz");
+     } else {
+        content = i;
+    }
+    
 }
 
 
