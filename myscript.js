a120c441c6ccffa7433e14185bfe141edf2fddae
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-21 17:33:52 +0100

    fix some details

--- myscript.js
+++ myscript.js
@@ -5,30 +5,32 @@ const container = document.querySelector(".container");
 let content;
 
 // number from 1 to 100
-for (let i = 1; i < 100; i++) {
+for (let i = 1; i <= 100; i++) {
     //console.log(i);
 
     // creazione elementi tramite innerHTML
-    const box = document.createElement("div");
-
-    // aggiunta classe 
-	box.classList.add("box");
-
-    
+    const box = document.createElement("div");    
 
    // se il numero è divisibile per 3 stampa "fizz", se è divisibile per 5 stampa "buzz", se è divisibile sia per 3 che per 5 stampa "fizzbuzz", se non è divisibile ne per 3 ne per 5 stampa solo il numero
    if (i % 3 === 0 && i % 5 === 0) {
+        // console.log("fizz.buzz");
         content = "fizz.buzz";
         box.classList.add("fizz_buzz")
     } else if (i % 3 === 0)  {
+        // console.log("fizz");
         content = "fizz";
         box.classList.add("fizz");
     } else if (i % 5 === 0) {
+        // console.log("buzz");
         content = "buzz";
         box.classList.add("buzz");
      } else {
+        // console.log(i);
         content = i;
     }
+
+    // aggiunta classe 
+	box.classList.add("box");
 
     // aggiunta contenuto all'elemento
     box.append(content);
@@ -37,5 +39,3 @@ for (let i = 1; i < 100; i++) {
     container.append(box);
     
 }
-
-
