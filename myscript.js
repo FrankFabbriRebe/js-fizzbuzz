50733a32e173cce425d4b9c745a930d55b5a4bc9
Francesco Fabbri Rebecchi <rebecchi.ff@gmail.com>
2023-11-21 16:17:25 +0100

    creazione elementi tramite innerHTML

--- myscript.js
+++ myscript.js
@@ -1,13 +1,33 @@
+// selezione elemento HTML 
+const container = document.querySelector(".container");
+
+// creazione variabile content
+let content;
+
 // number from 1 to 100
 for (let i = 1; i <= 100; i++) {
     //console.log(i);
 
    // se il numero è divisibile per 3 stampa "fizz", se è divisibile per 5 stampa "buzz", se è divisibile sia per 3 che per 5 stampa "fizzbuzz", se non è divisibile ne per 3 ne per 5 stampa solo il numero
     if (i % 3 === 0)  {
-        console.log("fizz");
+        content = "fizz";
     } else if (i % 5 === 0) {
-        console.log("buzz");
+        content = "buzz";
     } else {
-        console.log(i);
+        content = i;
     }
+
+    // creazione elementi tramite innerHTML
+    const box = document.createElement("div");
+
+    // aggiunta classe 
+	box.classList.add("box");
+
+    // aggiunta contenuto all'elemento
+    box.append(content);
+
+    // aggiunta dell'elelmento al container
+    container.append(box);
 }
+
+
